b3f6827266b76a0674c77db7f37b9176f96f250a
Mehdi <mehditeymorian322@gmail.com>
2021-11-27 10:42:55 +0330

    add timeout to redis connection

--- internal/cache/redis/redis.go
+++ internal/cache/redis/redis.go
@@ -2,7 +2,10 @@ package redis
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-redis/redis/v8"
+	"github.com/prometheus/common/log"
+	"time"
 )
 
 type Redis struct {
@@ -10,11 +13,19 @@ type Redis struct {
 }
 
 func New(ctx context.Context, cfg Config) (*redis.Client, error) {
+	timeout, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
 	client := redis.NewClient(&redis.Options{
 		Addr:     cfg.Address,
 		Password: cfg.Password,
 		DB:       cfg.DB,
 	})
+
+	log.Info("Connecting to redis...")
+	if err := client.Ping(timeout).Err(); err != nil {
+		return nil, fmt.Errorf("error connecting to redis: %v", err)
+	}
 
 	return client, nil
 }
