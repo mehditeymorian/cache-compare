7ee2a233988c4f5f28953438d46b3b0652743494
Mehdi <mehditeymorian322@gmail.com>
2021-11-27 10:47:45 +0330

    add mongodb handler

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"github.com/labstack/echo/v4"
+	"github.com/mehditeymorian/cache-compare/internal/cache/mongodb"
 	"github.com/mehditeymorian/cache-compare/internal/cache/redis"
 	"github.com/mehditeymorian/cache-compare/internal/cache/tikv"
 	"github.com/mehditeymorian/cache-compare/internal/config"
@@ -13,26 +14,39 @@ import (
 func Execute() {
 	ctx := context.Background()
 
+	// reading config
 	cfg := config.New("config.yaml")
 	log.Printf("config %+v\n", cfg)
 
+	// initializing tikv client
 	tikvClient, err := tikv.New(ctx, cfg.Tikv)
 	if err != nil {
 		log.Printf("error initializing tikv client: %v", err)
 	}
 	tikvCache := tikv.Tikv{Client: tikvClient}
 
+	// initializing redis client
 	redisClient, err := redis.New(ctx, cfg.Redis)
 	if err != nil {
 		log.Printf("error initializing redis client: %v", err)
 	}
 	redisCache := redis.Redis{Client: redisClient}
 
-	app := echo.New()
+	// initializing mongoDB client
+	mongoClient, err := mongodb.New(ctx, cfg.MongoDB)
+	if err != nil {
+		log.Printf("error initializing MongoDB client: %v", err)
+	}
+	mongoCache := mongodb.MongoDB{Client: mongoClient}
 
+	// creating http app
+	app := echo.New()
+	// registering handlers
 	handler.CacheHandler{Client: tikvCache}.Register("/tikv", app)
 	handler.CacheHandler{Client: redisCache}.Register("/redis", app)
+	handler.CacheHandler{Client: mongoCache}.Register("/mongo", app)
 
+	// starting server
 	if err := app.Start(":" + cfg.Http.Port); err != nil {
 		log.Fatalf("failed to start HTTP server! %v", err)
 	}
