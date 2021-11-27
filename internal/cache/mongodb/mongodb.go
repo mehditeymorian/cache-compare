95c93a1c24d8cccbfb7572a048f1defd829d3604
Mehdi <mehditeymorian322@gmail.com>
2021-11-27 10:47:27 +0330

    complete mongodb client

--- internal/cache/mongodb/mongodb.go
+++ internal/cache/mongodb/mongodb.go
@@ -2,19 +2,71 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
+	"github.com/mehditeymorian/cache-compare/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
 )
 
+const collectionName = "cache"
+
 type MongoDB struct {
+	Client *mongo.Database
+}
+
+func New(ctx context.Context, cfg Config) (*mongo.Database, error) {
+	// create mongodb connection
+	client, err := mongo.NewClient(options.Client().ApplyURI(cfg.Uri))
+	if err != nil {
+		return nil, fmt.Errorf("db new client error: %w", err)
+	}
+
+	// connect to the mongodb
+	{
+		ctx, done := context.WithTimeout(context.Background(), 10*time.Second)
+		defer done()
+
+		if err := client.Connect(ctx); err != nil {
+			return nil, fmt.Errorf("db connection error: %v", err)
+		}
+	}
+	// ping the mongodb
+	{
+		ctx, done := context.WithTimeout(context.Background(), 10*time.Second)
+		defer done()
+
+		if err := client.Ping(ctx, readpref.Primary()); err != nil {
+			return nil, fmt.Errorf("db ping error: %w", err)
+		}
+	}
+
+	return client.Database(cfg.Name), nil
 }
 
 func (m MongoDB) Set(ctx context.Context, key string, value string) error {
-	panic("implement me")
+	data := model.NewKVPair(key, value)
+	_, err := m.Client.Collection(collectionName).InsertOne(ctx, data)
+	return err
 }
 
 func (m MongoDB) Get(ctx context.Context, key string) (string, error) {
-	panic("implement me")
+
+	result := m.Client.Collection(collectionName).FindOne(ctx, bson.D{{"key", key}})
+
+	var kvpair model.KVPair
+
+	err := result.Decode(&kvpair)
+	if err != nil {
+		return "", err
+	}
+
+	return kvpair.Value, nil
 }
 
 func (m MongoDB) Del(ctx context.Context, key string) error {
-	panic("implement me")
+	_, err := m.Client.Collection(collectionName).DeleteOne(ctx, bson.M{"key": key})
+	return err
 }
